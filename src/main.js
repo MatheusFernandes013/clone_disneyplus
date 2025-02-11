b89986973a6da53f7f8865459c2cb0b5cb334eaf
Matheus <matheus.oliveirag@outlook.com.br>
2025-02-11 12:50:22 -0300

    Finalizando a sessao shows

--- src/main.js
+++ src/main.js
@@ -8,9 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
             const tab = document.querySelector(`[data-tab-id="${tabsTarget}"]`);
             hidenTabs();
             tab.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 })
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
 
 function hidenTabs() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
